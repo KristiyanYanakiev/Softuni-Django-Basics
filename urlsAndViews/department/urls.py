87336a4e822cab746cdf80c5ecd569f1e7789aa8
KristiyanYanakiev <yankris_92@yahoo.com>
2026-01-13 18:54:12 +0200

    chore: practicing concepts from urlsAndViews lecture

---
+++ urlsAndViews/department/urls.py
@@ -0,0 +1,20 @@
+
+
+from django.urls import path, include
+
+from department.views import home_view, show_department_by_name, index, slug_view, path_view, uuid_view, \
+    view_types_home_view
+
+urlpatterns = [
+    path('', home_view),
+
+    path('view_types/', view_types_home_view),
+
+    path('view_types/', include([
+        path('<uuid:uuid>/', uuid_view),
+        path('<int:id>/', index),
+        path('<slug:slug>/', slug_view),
+        path('<str:department_name>/', show_department_by_name),
+        path('<path:path>/', path_view),
+    ]))
+]
---
+++ urlsAndViews/department/views.py
@@ -0,0 +1,27 @@
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def home_view(request: HttpRequest) -> HttpResponse:
+    return HttpResponse('Hello World')
+
+def view_types_home_view(request):
+    return HttpResponse('Home page')
+
+def show_department_by_name(request, department_name: str):
+    context = {
+        'department_name': department_name
+    }
+    return render(request, 'department_by_name.html', context)
+
+def index(request, id: int):
+    return HttpResponse(f"The id is {id}")
+
+def slug_view(request, slug: str):
+    return HttpResponse(f"This is a slug view and the slug is: {slug}")
+
+def path_view(request, path: str):
+    return HttpResponse(f"This is a path review with the path: {path}")
+
+def uuid_view(request, uuid: str):
+    return HttpResponse(f"{uuid} is a {type(uuid)}", content_type='text/plain')
